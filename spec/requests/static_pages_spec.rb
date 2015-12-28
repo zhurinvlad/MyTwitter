845d8fff7b9698ef427d5627823775a39cfd30f8
zhurinvlad <vlad032_grax@mail.ru>
2015-12-28 12:40:31 +0300

    Helpers

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,13 +3,23 @@ require 'spec_helper'
 describe "Static pages" do
 
   subject { page }
-  describe "Home page" do
-  	before { visit root_path }
-    it { should have_content('Mytwitter')}
-    it { should have_title(full_title(''))}
-    it { should_not have_title('| Home')}
+#Аналогичный вариант
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
   end
 
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading)    { 'Mytwitter' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+  end
+
+
+#Аналогичный вариант
   describe "Help page" do
     before { visit help_path }
     it { should have_content('Help') }
@@ -22,18 +32,32 @@ describe "Static pages" do
     it { should have_title(full_title('About Us')) }
   end
 
-   #Аналогичный вариант
+#Аналогичный вариант
    let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-
   describe "Contact page" do
 	before { visit contact_path }
     it "should have the content 'Contact'" do
-      expect(page).to have_content('Contact')
+      expect(page).to have_selector('h1', text: 'Contact')
     end
 
     it "should have the title 'Contact'" do
       expect(page).to have_title("#{base_title} | Contact")
     end
+  end
+
+it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title('Sign up'))
   end
 
 end
